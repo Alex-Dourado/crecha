b4d79ef6f6351ac8e19441a208542fed35cd4386
Alex-Dourado <alexsandro_dourado@hotmail.com>
2024-08-19 14:30:56 -0300

    maisessi

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://google.com')
+    cy.get('#APjFqb').type("suco de maça com fazer{enter}")
+    
+   
   })
 })
